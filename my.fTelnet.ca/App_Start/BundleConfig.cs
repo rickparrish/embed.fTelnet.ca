c377bd5c78f91fa2b02ccf454a99a7772311153b
rickparrish <github@rick.parrish>
2014-02-27 19:49:14 -0500

    Added bootstrap 3.1.1 and jquery 1.11.0

--- my.fTelnet.ca/App_Start/BundleConfig.cs
+++ my.fTelnet.ca/App_Start/BundleConfig.cs
@@ -11,7 +11,14 @@ namespace my.fTelnet.ca
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/css/bootstrap.min.css",
+                "~/Content/css/bootstrap-theme.min.css",
+                "~/Content/css/site.css"));
 
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/jquery-1.11.0.min.js",
+                "~/Scripts/bootstrap.min.js"));
         }
     }
 }
