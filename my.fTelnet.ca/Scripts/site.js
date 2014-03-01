26c0a09e5903c8034807748d232f1a6a963164a8
rickparrish <github@rick.parrish>
2014-02-28 20:30:46 -0500

    Update font when viewport size changes

--- my.fTelnet.ca/Scripts/site.js
+++ my.fTelnet.ca/Scripts/site.js
@@ -1,7 +1,15 @@
-﻿$(document).ready(function () {
-    SetFontSize(12, 23) || SetFontSize(10, 19) || SetFontSize(9, 16) || SetFontSize(8, 13) || SetFontSize(8, 12) || SetFontSize(7, 12) || SetFontSize(6, 8, true);
+﻿var SiteInitted = false;
+
+$(document).ready(function () {
+    SetBestFontSize();
     HtmlTerm.Init("HtmlTermContainer", ClientVars);
     VK_Init();
+
+    SiteInitted = true;
+});
+
+$(window).resize(function () {
+    if (SiteInitted) SetBestFontSize();
 });
 
 function OpenPanel(id) {
@@ -9,12 +17,20 @@ function OpenPanel(id) {
     $('#pnl' + id).show('slow');
 }
 
+function SetBestFontSize() {
+    SetFontSize(12, 23) || SetFontSize(10, 19) || SetFontSize(9, 16) || SetFontSize(8, 13) || SetFontSize(8, 12) || SetFontSize(7, 12) || SetFontSize(6, 8, true);
+}
+
 function SetFontSize(width, height, force) {
     if (typeof force === 'undefined') force = false;
 
     if (force || (($(window).width() >= (width * 80)) && (($(window).height() - 60) >= (height * 25)))) { // -60 for top nav
-        ClientVars.FontWidth = width;
-        ClientVars.FontHeight = height;
+        if (SiteInitted) {
+            Crt.SetFont(437, width, height);
+        } else {
+            ClientVars.FontWidth = width;
+            ClientVars.FontHeight = height;
+        }
         return true;
     }
 
